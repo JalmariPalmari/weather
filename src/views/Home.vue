48241692647a076f90435443bf489c45e1a93968
JalmariPalmari <puura.janne@gmail.com>
2020-03-06 00:06:06 +0200

    1

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <weatherWidget />
+  </div>
+</template>
+
+<script>
+import weatherWidget from '@/components/weatherWidget.vue'; // import component
+
+export default {
+  name: "home",
+  components: { // and register it
+    weatherWidget
+  }
+}
+</script>
+
+<style lang="scss" scoped> // some styles 🖍️
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
